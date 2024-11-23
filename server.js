2989cd4866c2d8103e72f0464271a4dee0fce2d2
nicksarahh <nicolysarahfeltri@gmail.com>
2024-11-23 20:47:18 -0300

    poliwear

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = mysql.createPool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
-  database: process.env.DB_PORT,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
